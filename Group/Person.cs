7a57a8d05df10834a0cf096301c6f03d960f1de5
Svad_0tmq@student.itstep.org <Svad_0tmq@student.itstep.org>
2022-10-21 17:57:06 +0300

    upd

--- Group/Person.cs
+++ Group/Person.cs
@@ -1,25 +1,77 @@
 ﻿namespace Group
 {
-    
+    struct DateBirthday
+    {
+        int day;
+        int month;
+        int year;
+
+        public int Day
+        {
+            get { return day; }
+            set { day = value; }
+        }
+        public int Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+    }
+    struct Address
+    {
+        string country;
+        string city;
+        string street;
+        int house;
+
+        public string Country
+        {
+            get { return country; }
+            set { country = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string Street
+        {
+            get { return street; }
+            set { street = value; }
+        }
+        public int House
+        {
+            get { return house; }
+            set { house = value; }
+        }
+    }
 
     internal class Person 
     {
-        private string name;
-        private int age;
-        private string surname;
-        private DateTime dateBirthday;
-        private string address;
-        private int phoneNumber;
+        string name;
+        int age;
+        string surname;
+        int phoneNumber;
+        Address address;
+        DateBirthday dateBirthday;
 
         public Person()
         {
             name = "Andrey";
             age = 20;
             surname = "Alexeev";
-            dateBirthday.AddMonths(2);
-            dateBirthday.AddDays(23);
-            dateBirthday.AddYears(2002);
-            address = "T.Shevchenka 23";
+            dateBirthday.Month = 2;
+            dateBirthday.Day = 23;
+            dateBirthday.Year = 2002;
+            address.Country = "Ukraine";
+            address.City = "Odessa";
+            address.Street = "Deribasovskaya";
+            address.House = 22;
             phoneNumber = 5553535;
         }
         /// <summary>
@@ -107,23 +159,9 @@
         /// <param name="day">Передача дня</param>
         public void SetBirthday(int year, int month, int day)
         {
-            dateBirthday = new DateTime(year, month, day);
-        }
-
-        /// <summary>
-        /// Метод который позволяет поменять адрес студенту
-        /// </summary>
-        /// <param name="adress">Передача адреса</param>
-        public string Address
-        {
-            get
-            {
-                return address;
-            }
-            set
-            {
-                address = value;
-            }
+            dateBirthday.Year = year;
+            dateBirthday.Month = month;
+            dateBirthday.Day = day;
         }
         /// <summary>
         /// Метод который позволяет поменять телефонный номер студенту
